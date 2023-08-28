da241027d8a5d4e95acbb3490a7601c90eb5eaea
Kavya Sai Isheka <yakkalakavyasaiisheka@gmail.com>
2023-08-28 14:00:15 +0530

    Completed

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -21,7 +21,7 @@ export default async function TopicsList() {
     const { topics } = await getTopics();
     return (
         <>
-      {topics.map((t) => (
+      {data?.topics?.map((t) => (
         <div
           key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
